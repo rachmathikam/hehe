ee74aebd74c4b5a3f44ed85af190b4a169a91156
Syarif Hidayatullah <dsyarif39@gmail.com>
2022-09-01 12:34:37 +0700

    update edit

--- resources/views/pages/guru/edit.blade.php
+++ resources/views/pages/guru/edit.blade.php
@@ -28,17 +28,17 @@
                             @csrf
                             @method('PUT')
                             <div class="mb-3">
-                                <h7 class="pb-3 mb-0">NIP</h7>
+                                <label class="pb-3 mb-0">NIP</label>
                                 <input class="form-control" type="text" value="{{ $data->nip }}" name="nip"
                                     placeholder="your Nip">
                             </div>
                             <div class="mb-3">
-                                <h7 class="pb-3 mb-0">Nama</h7>
+                                <label class="pb-3 mb-0">Nama</label>
                                 <input class="form-control" type="text" value="{{ $data->nama }}"name="nama"
                                     placeholder="Nama" />
                             </div>
                         <div class="mt-4 mb-4">
-                            <h7 class="pb-3 mb-0">Tempat, Tanggal Lahir</h7>
+                            <label class="pb-3 mb-0">Tempat, Tanggal Lahir</label>
                             <div class="row">
                                 <div class="col-md-6">
                                     <input type="text" class="form-control" name="tempat_lahir" value="{{ $data->tempat_lahir }}" placeholder="">
@@ -50,14 +50,14 @@
                         </div>
 
                         <div class="mt-4 mb-4">
-                            <h7 class="pb-3 mb-0">Agama</h7>
+                            <label class="pb-3 mb-0">Agama</label>
                             <div class="col-auto">
                                 <input class="form-control" type="text" value="{{ $data->agama }}" name="agama"
                                     placeholder="Agama" />
                             </div>
                         </div>
                         <div class="mt-4 mb-4">
-                            <h7 class="pb-3 mb-0">Jenis Kelamin</h7>
+                            <label class="pb-3 mb-0">Jenis Kelamin</label>
                             <div class="col-auto">
                                 <input class="form-control" type="text" value="{{ $data->gender }}"
                                     name="jenis_kelamin" placeholder="Jenis Kelamin" />
@@ -66,17 +66,17 @@
                         <div class="mt-4 mb-4">
                             <div class="row">
                                 <div class="col-md-6">
-                                    <h7 class="pb-3 mb-0">Role</h7>
+                                    <label class="pb-3 mb-0">Role</label>
                                     <input type="text" class="form-control" name="tempat_lahir" value="{{ $data->user->role->role }}" placeholder="">
                                 </div>
                                 <div class="col-md-6">
-                                    <h7 class="pb-3 mb-0">Mengajar</h7>
+                                    <label class="pb-3 mb-0">Mengajar</label>
                                     <input type="text" class="form-control" name="tanggal_lahir"  value="{{ $data->mapel->name }}">
                                 </div>
                             </div>
                         </div>
                         <div class="mt-4 mb-4">
-                            <h7 class="pb-3 mb-0">No Telphone</h7>
+                            <label class="pb-3 mb-0">No Telphone</label>
                             <div class="col-auto">
                                 <input class="form-control" type="text" value="{{ $data->no_telp }}" name="no_telp"
                                     placeholder="Jenis Kelamin" />
@@ -86,7 +86,7 @@
                             <div class="row">
                                 <div class="col">
                                     <div>
-                                        <h7 class="pb-3 mb-0">Alamat</h7>
+                                        <label class="pb-3 mb-0">Alamat</label>
                                         <textarea class="form-control" id="exampleFormControlTextarea4" name="alamat">{{ $data->alamat }}</textarea>
                                     </div>
                                 </div>
@@ -95,7 +95,7 @@
                     </div>
                     <div class="card-footer">
                         <button class="btn btn-primary" type="submit">Submit</button>
-                        <button class="btn btn-secondary">Cancel</button>
+                        <a  href="{{ route('guru.index') }}" class="btn btn-secondary" >Cancel</a>
                      </form>
                     </div>
                 </div>
--- resources/views/pages/guru/index.blade.php
+++ resources/views/pages/guru/index.blade.php
@@ -20,46 +20,101 @@
         </div>
         <div class="card-body">
             <div class="table-responsive">
-                <table class="display" id="basic-2">
+                <table class="display table-guru" id="basic-2">
                     <thead>
                         <tr>
                             <th>No</th>
                             <th>Nip</th>
                             <th>Name</th>
-                            <th>Role</th>
                             <th>Mengajar</th>
                             <th>Action</th>
                         </tr>
                     </thead>
                     <tbody>
-                        @foreach ($data as $guru)
-
-                        <tr>
-                            <td>{{ $loop->iteration }}</td>
-                            <td>{{ $guru->nip }}</td>
-                            <td>{{ $guru->nama }}</td>
-                            <td>{{ $guru->user->role->role }}</td>
-                            <td>{{ $guru->mapel->name }}</td>
-                            <td>
-                                <form onsubmit="return confirm('Apakah Anda Yakin ?');" action="{{ route('guru.destroy', $guru->id) }}" method="POST">
-                                 <a href="{{ route('guru.show', $guru->id) }}"><button class="btn btn-primary btn-xs" type="button">
-                                    <i class="fa-solid fa-eye"></i>
-                                </button></a>
-                                <a href="{{ route('guru.edit', $guru->id) }}"><button class="btn btn-secondary btn-xs" type="button">
-                                    <i class="fa-solid fa-pen-to-square"></i>
-                                </button></a>
-                                @csrf
-                                @method('DELETE')
-                                <button class="btn btn-danger btn-xs" type="submit">
-                                    <i class="fa-solid fa-trash"></i>
-                                </button>
-                            </td>
-                        </tr>
-                        @endforeach
                     </tbody>
                 </table>
             </div>
         </div>
     </div>
 </div>
+
+<script>
+    $(document).ready(function() {
+            $('.table-guru').DataTable({
+                processing: true,
+                serverside: true,
+                ajax: {
+                    url: "{{route('guru.index')}}",
+                    type: 'GET'
+                },
+                responsive: true,
+                columns: [{
+                        data: 'DT_RowIndex',
+                    },
+                    {
+                        data: 'nip',
+                    },
+                    {
+                        data: 'nama',
+                    },
+                    {
+                        data: 'mapel'
+                    },
+                    {
+                        data: 'action',
+                    },
+                ]
+            })
+        })
+
+    function deleteItem(e) {
+            let id = e.getAttribute('data-id');
+            let name = e.getAttribute('data-name');
+            const swalWithBootstrapButtons = Swal.mixin({
+                customClass: {
+                    confirmButton: 'btn btn-success',
+                    cancelButton: 'btn btn-danger'
+                },
+                buttonsStyling: true
+            });
+            swalWithBootstrapButtons.fire({
+                title: 'Yakin menghapus '+name+" ?",
+                text: name+" akan di hapus",
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonText: 'Ya,Hapus',
+                cancelButtonText: 'Tidak, Batal!',
+                reverseButtons: true
+            }).then((result) => {
+                if (result.value) {
+                    if (result.isConfirmed) {
+                        $.ajax({
+                            type: 'POST',
+                            url: "guru" + id,
+                            data: {
+                                "_token": "{{ csrf_token() }}",
+                                "_method": 'DELETE',
+                            },
+                            success: function(response) {
+                                if (response.success) {
+                                    toastr.success(name + response.message);
+                                    var oTable = $('.table-guru').DataTable(); //inialisasi datatable
+                                    oTable.ajax.reload();
+                                }
+                            }
+
+                        });
+                    }
+                } else if (
+                    result.dismiss === Swal.DismissReason.cancel
+                ) {
+                    swal.fire(
+                        'Batal',
+                        'Data '+name+' tidak dihapus',
+                        'error'
+                    )
+                }
+            });
+        }
+</script>
 @endsection
